c0ff8ab646b21e732beae8252bc54041fa7fddd5
Ilia R <ilya5857678@mail.ru>
2021-07-21 17:16:12 +0100

    Setting up xlsx read

--- main.py
+++ main.py
@@ -7,10 +7,19 @@ from openpyxl.chart import BarChart, Reference
 def process_workbook(filename):
     wb = xl.load_workbook(filename)
     sheet = wb['Sheet1']
-    for item in range(10):
+    for item in range(2):
         randword = random.randint(1,sheet.max_row - 1 )
         cell = sheet.cell(randword,1)
         print(cell.value)
+        print(input('Enter in the following format: präteretum perfect english '))
+        output = ' '
+        cell2 = sheet.cell(randword, 2)
+        cell3 = sheet.cell(randword, 3)
+        cell4 = sheet.cell(randword, 4)
+        cell5 = sheet.cell(randword, 5)
+        output = cell2.value + '_|_' + cell3.value + '_|_' + cell4.value + '_|_' + cell5.value
+        print(output)
+
 
     # for row in range(2, sheet.max_row + 1):
     #     cell = sheet.cell(row,3)
