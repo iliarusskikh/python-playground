065badd3c3462894071195053f9ae967e9bb02e9
iliarusskikh <86166676+iliarusskikh@users.noreply.github.com>
2025-06-25 19:38:08 +0100

    work

--- intro/basics/codecamp0.py
+++ intro/basics/codecamp0.py
@@ -66,5 +66,56 @@ class RPS(Enum):
     SCISSORS = 3
     
 #print(RPS(2))
-print(RPS['ROCK']
-print(RPS.ROCK.value)#1
+#print(RPS['ROCK']
+#print(RPS.ROCK.value)#1
+
+
+
+#lsit and tuples
+list12 = ['Dave', True, 42]
+
+#print("Dave" in list12)
+print(list12.index('Dave'))
+#len(list12)
+#list12.append('NewItem')
+#list12 += ['Hold'] # need sqr br otherwise it would add each char
+
+list12.extend(['Robert', 'Jason'])#adds another list
+
+list12.insert(0,'Bob')#add to specific position
+list12[2:2] = ['Eddie', 'Alex']#adding new values, not replacing
+list12.remove('Bob')
+#.pop() removes last value in a list + returns this item
+
+list12.sort()
+list12.sort(key=str.lower) #this would include lower cases in the sorting algo
+
+del list12[0] #removed item
+list12.clear() #keeps the list but clears its content
+del list12 #delete entire list
+
+nums = [1,2,5,67,2,4,6,7]
+nums.reverse()
+nums.sort(reverse=True)
+
+#print(sorted(nums, reverse=True))# does not modifies the list
+
+#make a copy
+numscopy = nums.copy()
+mynums = list(nums)
+mycopy = nums[:]
+
+#tuple - unchangable data
+mytup = tuple(('Dave',43,True))
+anothertup = (1,3,5,6)
+newlist = list(mytup)
+newlist.append('NewValue')
+newtuple = tuple(newlist)
+
+(one,two, *three) = anothertup #unpacking
+
+#.count(1) -> how many occurences
+
+
+
+#dictionaries
